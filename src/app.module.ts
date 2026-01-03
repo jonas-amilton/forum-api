dada83f3849e4bdfe28c62becd216457381cbbed
jonas-amilton <jonas96@edu.unisinos.br>
2026-01-03 20:00:13 -0300

    fix: configurar ConfigModule e centralizar conexão com o banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
