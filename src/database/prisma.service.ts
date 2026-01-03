108533cfcd37eb59d5708d0dbcf084ac027855a0
jonas-amilton <jonas96@edu.unisinos.br>
2026-01-03 19:54:55 -0300

    feat: cria modulo database

---
+++ src/database/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      adapter: new PrismaBetterSqlite3({
+        url: process.env.DATABASE_URL!,
+      }),
+    });
+  }
+}
