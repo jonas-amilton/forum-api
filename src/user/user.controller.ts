44a2a104f5fd95ed259c88bc07670829154b34a5
jonas-amilton <jonas96@edu.unisinos.br>
2026-01-03 20:39:49 -0300

    feat: cria endpoints de buscar, atualizar e remover usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -20,4 +20,22 @@ export class UserController {
   ): Promise<UserModel> {
     return this.userService.createUser(userData);
   }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<UserModel | null> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() userData: Prisma.UserUpdateInput) {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: userData,
+    });
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
+  }
 }
