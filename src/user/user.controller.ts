15ec59498b85f1d386ea089f11fecca29c441199
jonas-amilton <jonas96@edu.unisinos.br>
2026-01-05 22:40:27 -0300

    feat: implementa guard nos endpoints

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,9 +6,11 @@ import {
   Body,
   Put,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { Prisma, User as UserModel } from '@prisma/client';
 import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
 export class UserController {
@@ -21,19 +23,25 @@ export class UserController {
     return this.userService.createUser(userData);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async getUser(@Param('id') id: string): Promise<UserModel | null> {
     return this.userService.user({ id: Number(id) });
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
-  async updateUser(@Param('id') id: string, @Body() userData: Prisma.UserUpdateInput) {
+  async updateUser(
+    @Param('id') id: string,
+    @Body() userData: Prisma.UserUpdateInput,
+  ) {
     return this.userService.updateUser({
       where: { id: Number(id) },
       data: userData,
     });
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteUser(@Param('id') id: string): Promise<UserModel> {
     return this.userService.deleteUser({ id: Number(id) });
