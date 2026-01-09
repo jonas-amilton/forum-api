c1b98f59998694af5924b34f1377359e79d782da
jonas-amilton <jonas96@edu.unisinos.br>
2026-01-08 23:06:39 -0300

    feat: atualiza modulos importados

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { QuestionsModule } from './questions/questions.module';
+import { AnswersModule } from './answers/answers.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { QuestionsModule } from './questions/questions.module';
     UserModule,
     AuthModule,
     QuestionsModule,
+    AnswersModule,
   ],
 })
 export class AppModule {}
